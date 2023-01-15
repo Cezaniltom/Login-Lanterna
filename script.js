29fa1a8aeb7be8caba503bf767097d0fad9db3ca
Cezaniltom Silva <91354726+Cezaniltom@users.noreply.github.com>
2023-01-15 15:33:53 -0300

    verificando

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const passwordInput = document.getElementById('password');
 
 root.addEventListener('mousemove', (e) => {
   let rect = beam.getBoundingClientRect();
-  let mouseX = rect.right + (rect.width / 2);
+  let mouseX = rect.right + (rect.width / 2); 
   let mouseY = rect.top + (rect.height / 2);
   let rad = Math.atan2(mouseX - e.pageX, mouseY - e.pageY);
-  let degres = (rad * (20/Math.PI) * -1) - 350;
+  let degrees = (rad * (20 / Math.PI) * -1) - 350;
 
   root.style.setProperty('--beamDegrees', `${degrees}deg`);
 });
